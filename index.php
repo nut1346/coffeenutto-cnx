218e9805debc4ac2cc54fbe39366ada870209959
nut1346 <42630759+nut1346@users.noreply.github.com>
2018-09-20 11:41:41 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-สวัสสดี
+
 <?php
 $servername = "us-cdbr-iron-east-01.cleardb.net";
 $username = "b29f0890a17ad7";
@@ -11,6 +11,42 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
- 
-$conn->close();
+
+$data = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($data,true));
+
+var_dump(  $result = $conn->query("SELECT * FROM menu"));
+
+echo $data;
+
+if ($data['queryResult']['intent']['displayName']=="Default Welcome Intent"){
+    var_dump($result,true);
+
+
+
+
+   /* $output = [
+        "fulfillmentText" => "set developer channel",
+        "fulfillmentMessages" => [
+            [
+                "text" => 
+                [
+                    "text" => 
+                    [
+                       "โกโก้".$result[0]->name
+                    ]
+                ]
+            ]
+        ]
+    ];
+
+
+   // echo json_encode($output);
+}else{
+   */
+}
+
+
+
+
 ?> 
